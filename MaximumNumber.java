7c959b976aca5c55ece8889a15bb476840f0d8c8
vishangi <vishangid@gmail.com>
2022-06-18 18:20:37 +0530

    added : print the maximum number using generic mathod

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,28 +1,34 @@
 package com.brigelabz.maximum;
 
-public class MaximumNumber {
+import java.util.Arrays;
 
-	private static Integer testMax(Integer x, Integer y, Integer z ) {
-		Integer a = x;
-		Integer b = y;
-		Integer c = z;
-		Integer max = a;
-		
-		if(b.compareTo(max)>0) {
-			max = b; 
-		}
-		if(c.compareTo(max)>0) {
-			max = c; 
-		}
+public class MaximumNumber<E> {
+
+	public E testMax(E... a) {
+
+		// Using array method & Sorting the all numbers to find maximum there is no
+		// limit
+		Arrays.sort(a);
+		E max = a[a.length - 1];
+		printMax(max);
 		return max;
+	}
 
+	private void printMax(E max) {
+		System.out.println(max);
 	}
 
 	public static void main(String[] args) {
-		int result = MaximumNumber.testMax(-10,-20,-30);
-		System.out.println("Maximum number is :"+result);
+		System.out.println("Welcome to maximum string program !");
 		
+		MaximumNumber<Integer> integerMaximum = new MaximumNumber<>();
+		integerMaximum.testMax(-10, -20, -30, 50, 20);
 		
-	}
+		MaximumNumber<Float> floatMaximum = new MaximumNumber<>();
+		floatMaximum.testMax(10.5f, 15.2f, 5.2f, 2.3f, 4.5f);
+		
+		MaximumNumber<String> stringMaximum = new MaximumNumber<>();
+		stringMaximum.testMax("apple", "banana", "mango", "Custardapple", "coconut");
 
+	}
 }
