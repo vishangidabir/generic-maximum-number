49dec0725be5f13a0fdd381ab6c2f2e90b53165d
vishangi <vishangid@gmail.com>
2022-06-18 15:56:07 +0530

    added : refactor using Genrics Class Finding Maximum

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,28 +1,42 @@
 package com.brigelabz.maximum;
 
-public class MaximumNumber {
+public class MaximumNumber<E extends Comparable<E>> {
+	private E a, b,c;
+	
+	public MaximumNumber(E a, E b, E c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 
-	private static Integer testMax(Integer x, Integer y, Integer z ) {
-		Integer a = x;
-		Integer b = y;
-		Integer c = z;
-		Integer max = a;
-		
-		if(b.compareTo(max)>0) {
-			max = b; 
+	public E testMax() {
+		return MaximumNumber.testMax(a, b, c);
+	}
+
+	// determine largest three comparable objects
+	public static <E extends Comparable<E>> E testMax(E a, E b, E c) {
+
+		E max = a; // assume a is initially largest
+
+		if (b.compareTo(max) > 0) {
+			max = b; // b is largest so far
 		}
-		if(c.compareTo(max)>0) {
-			max = c; 
+		if (c.compareTo(max) > 0) {
+			max = c; // c is largest so far
 		}
-		return max;
+
+		return max; // return largest element
 
 	}
 
 	public static void main(String[] args) {
-		int result = MaximumNumber.testMax(-10,-20,-30);
-		System.out.println("Maximum number is :"+result);
-		
-		
-	}
+		System.out.println("Welcome to maximum string program !");
+		MaximumNumber<Integer> maximumNumber = new MaximumNumber<>(-10, -20, -30);
+		MaximumNumber<Float> maximumNumber1 = new MaximumNumber<>(10.5f, 15.2f, 5.2f);
+		MaximumNumber<String> maximumNumber2 = new MaximumNumber<>("apple", "banana", "peach");
+		System.out.println("\nMaximum integer among three integer : "+maximumNumber.testMax());
+		System.out.println("Maximum float among three float : "+maximumNumber1.testMax());
+		System.out.println("Maximum string among three float : "+maximumNumber2.testMax());
 
+	}
 }
